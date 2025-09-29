60440152a62cbdf9409c6e2b1a1c4490dc6f2e2f
Alejandro Mata <alemalvarez@icloud.com>
2025-09-29 18:35:36 +0200

    Add custom sweep script and update configuration references
    - Introduced custom_sweep.py to facilitate model training with the DeeperCustom architecture, allowing for flexible parameter parsing and validation.
    - Updated sweep_custom.yaml to reference the new custom sweep script for improved clarity in experiment configurations.
    - Modified train.py to utilize the CustomConfig class for enhanced type safety and clarity in model configuration.
    - Ensured consistent handling of model parameters and validation checks throughout the new implementation.

--- experiments/AD_vs_HC/combined/raw/train.py
+++ experiments/AD_vs_HC/combined/raw/train.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 from core.raw_dataset import RawDataset
 from core.schemas import BaseModelConfig
-from models.simple_2d import Deeper2D
+from models.simple_2d import DeeperCustom
 from core.train_model import train_model
 from dotenv import load_dotenv
 import os
@@ -19,7 +19,9 @@ logger.info(f"H5 file path: {H5_FILE_PATH}")
 WANDB_PROJECT = "AD_vs_HC"
 # WANDB_CONFIG = yaml.load(open("experiments/AD_vs_HC/combined/raw/simple2d3layers.yaml"), Loader=yaml.FullLoader)
 # WANDB_CONFIG = yaml.load(open("experiments/AD_vs_HC/combined/raw/improved2d.yaml"), Loader=yaml.FullLoader)
-WANDB_CONFIG = yaml.load(open("experiments/AD_vs_HC/combined/raw/deeper.yaml"), Loader=yaml.FullLoader)
+# WANDB_CONFIG = yaml.load(open("experiments/AD_vs_HC/combined/raw/deeper.yaml"), Loader=yaml.FullLoader)
+WANDB_CONFIG = yaml.load(open("experiments/AD_vs_HC/combined/raw/custom.yaml"), Loader=yaml.FullLoader)
+
 WANDB_CONFIG["random_seed"] = int(os.getenv("RANDOM_SEED", "42"))
 
 torch.manual_seed(int(os.getenv("RANDOM_SEED", "42")))
@@ -54,12 +56,27 @@ class Deeper2DConfig(BaseModelConfig):
     pos_weight: float
     paddings: List[Tuple[int, int]]
 
+@dataclass
+class CustomConfig(BaseModelConfig):
+    n_filters: List[int]
+    kernel_sizes: List[Tuple[int, int]]
+    strides: List[Tuple[int, int]]
+    dropout_rate: float
+    normalize: Literal['sample-channel', 'sample', 'channel-subject', 'subject', 'channel', 'full']
+    pos_weight: float
+    paddings: List[Tuple[int, int]]
+    activation: str
+    dropout_before_activation: bool
+
+
 # config = Simple2D3LayersConfig(**WANDB_CONFIG) # type: ignore
 # model = Simple2D3Layers(n_filters=config.n_filters, kernel_sizes=config.kernel_sizes, strides=config.strides, dropout_rate=config.dropout_rate)
 
 # config = Improved2DConfig(**WANDB_CONFIG) # type: ignore
-config = Deeper2DConfig(**WANDB_CONFIG) # type: ignore
-model = Deeper2D(n_filters=config.n_filters, kernel_sizes=config.kernel_sizes, strides=config.strides, dropout_rate=config.dropout_rate, paddings=config.paddings)
+config = CustomConfig(**WANDB_CONFIG) # type: ignore
+# model = Deeper2D(n_filters=config.n_filters, kernel_sizes=config.kernel_sizes, strides=config.strides, dropout_rate=config.dropout_rate, paddings=config.paddings)
+
+model = DeeperCustom(n_filters=config.n_filters, kernel_sizes=config.kernel_sizes, strides=config.strides, dropout_rate=config.dropout_rate, paddings=config.paddings, activation=config.activation, dropout_before_activation=config.dropout_before_activation)
 
 training_dataset = RawDataset(
     h5_file_path=H5_FILE_PATH,
